08bf6ea38b14263717c4c4b0373f1c48f7da9a55
Auxnos <71796281+Auxnos@users.noreply.github.com>
2021-07-09 21:24:59 -0700

    while

--- fake-char-server.lua
+++ fake-char-server.lua
@@ -508,7 +508,7 @@ if not falling then
         rightarm.CFrame = torso.CFrame * cn(1.5, 2, 0) * CFrame.Angles(rad(180),0,0)
         leftarm.CFrame = torso.CFrame * cn(-1.5, 2, 0) * CFrame.Angles(rad(180),0,0)
         rightleg.CFrame =torso.CFrame * cn(0.5, -2 0) * math.sin(sine/25), 0)
-        leftleg.CFrame = torso.CFrame * cn(-0.5, -2- 0 * math.sin(sine/25), 0)
+        leftleg.CFrame = torso.CFrame * cn(-0.5, -2, 0) * math.sin(sine/25), 0)
   end
     pcall(function()
         timepos = song.TimePosition
