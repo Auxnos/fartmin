c7e193f087836cd7174e15d999b22d9f5da39b67
Auxnos <71796281+Auxnos@users.noreply.github.com>
2021-07-09 21:10:56 -0700

    e

--- fake-char-server.lua
+++ fake-char-server.lua
@@ -496,12 +496,12 @@ headmesh.Scale = Vector3.new(1.25, 1.25, 1.25)
         rightleg.CFrame = torso.CFrame * cn(0.5, -2, .76 * math.sin(sine/25)) * CFrame.Angles(math.rad(-76*math.sin(sine/25)),0,0)
         leftleg.CFrame = torso.CFrame * cn(-0.5, -2, -.76 * math.sin(sine/25)) * CFrame.Angles(math.rad(76*math.sin(sine/25)),0,0)
     else
-        torso.CFrame = mainposz * cn(0, 0 + 0.5 * math.sin(sine/25), 0) * CFrame.Angles(0, math.rad(180),0)
+        torso.CFrame = mainposz * cn(0, 0 + 0.5 * math.sin(sine/25), 0) * CFrame.Angles(math.rad(5-3*math.cos(sine/14)), math.rad(180),0)
         head.CFrame = torso.CFrame * cn(0, 1.5, 0) * CFrame.Angles(math.rad(-5 + 10 * math.sin(sine/25)),0,0)
-        rightarm.CFrame = torso.CFrame * cn(1.5, 0, 0)
-        leftarm.CFrame = torso.CFrame * cn(-1.5, 0, 0)
-        rightleg.CFrame =torso.CFrame * cn(0.5, -2- 0.5 * math.sin(sine/25), 0)
-        leftleg.CFrame = torso.CFrame * cn(-0.5, -2- 0.5 * math.sin(sine/25), 0)
+        rightarm.CFrame = torso.CFrame * cn(1.5, 0, 0)* CFrame.Angles(math.rad(0), rad(15),0)
+        leftarm.CFrame = torso.CFrame * cn(-1.5, 0, 0)* CFrame.Angles(math.rad(0), rad(-15),0)
+        rightleg.CFrame =torso.CFrame * cn(0.5, -2- 0.5 * math.sin(sine/25), 0)* CFrame.Angles(math.rad(5+3*math.cos(sine/14)),0,0)
+        leftleg.CFrame = torso.CFrame * cn(-0.5, -2- 0.5 * math.sin(sine/25), 0)* CFrame.Angles(math.rad(5+3*math.cos(sine/14)),0,0)
     end
     pcall(function()
         timepos = song.TimePosition
