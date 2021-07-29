ad3ac99e18d211e9abaa7e85fa662ce311572339
Auxnos <71796281+Auxnos@users.noreply.github.com>
2021-07-28 19:51:43 -0700

    sa

--- ok.lua
+++ ok.lua
@@ -1,9 +1,8 @@
-wait(0.5)
 owner.Character.Archivable = true
 local char = owner.Character:Clone()
 char.Parent = nil
 local MainPos = CFrame.new(0, 15, 0)
-NewChar = owner.Character
+local Parts = {}
 local mainpos = CFrame.new()
 local Remote = Instance.new("RemoteEvent",owner)
 Remote.Name="FakeCharRemote"
@@ -15,7 +14,6 @@ local W,A,S,D = false, false, false, false
 local UserInputService = {}
 UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
 Remote.OnServerEvent:Connect(function(Player,Method,Args)
-    
     if Method == "CameraCFrame" then
         CameraCFrame,UserInputService.MouseBehavior = unpack(Args)
     elseif Method == "KeyUp" then
@@ -37,6 +35,8 @@ Remote.OnServerEvent:Connect(function(Player,Method,Args)
             S = true
         elseif Args == "d" then
             D = true
+        elseif Args == "f" then
+            FlyMode = not FlyMode
         end
     end
 end)
@@ -50,16 +50,14 @@ end)
 mouse.KeyUp:Connect(function(Key)
 pcall(owner.FakeCharRemote.FireServer, owner.FakeCharRemote, "KeyUp", Key)
 end)
+owner.FakeCharRemote.OnClientEvent:Connect(function(method,func)
+if method =="cam" then workspace.CurrentCamera.CameraSubject = func end
+end)
 game:GetService'RunService'.RenderStepped:Connect(function()
 pcall(function()
-for _,v in pairs(workspace:GetDescendants()) do
-if v:GetAttribute(owner.Name) == true then
-workspace.CurrentCamera.CameraSubject = v
-end
-end
 workspace.CurrentCamera.CameraType = "Custom"
 end)
-pcall(owner.FakeCharRemote.FireServer, owner.FakeCharRemote, "CameraCFrame", {workspace.CurrentCamera.CFrame,game:GetService'UserInputService'.MouseBehavior})
+pcall(owner.FakeCharRemote.FireServer, owner.FakeCharRemote, "CameraCFrame", {workspace.CurrentCamera.CFrame,game:GetService'UserInputService'.MouseBehavior,mouse.Hit})
 end)
 warn("Client loaded!")
 ]],owner.PlayerGui)
@@ -68,12 +66,13 @@ movementfalse = false
 flying = false
 falling = false
 local fallingspeed = 0
+b = {}
 local start = -tick()
 thing = 0
 LastThing = tick()
 mainpos = mainpos*CFrame.new(0, 25, 0)
 function cler(args,speed)
-    local chara = owner.Character
+    local chara = b.Character
     pcall(function()
         chara.Torso.CFrame = chara.Torso.CFrame:Lerp(MainPos * args[1],speed)
         chara.Head.CFrame = chara.Head.CFrame:Lerp(MainPos * args[1] * args[2],speed)
@@ -96,8 +95,19 @@ function thread(u1)
     end)
     u2 = u2()
 end
-b = {}
+for i,v in pairs(char:GetDescendants()) do
+    if v:IsA'Humanoid' then v:Destroy() end
+    if v:IsA'SpecialMesh' or v:IsA'BodyColors' or v:IsA("Accessory") or v:IsA("LocalScript") then v:Destroy() end
+    if v:IsA("BasePart") then v.Material = "Neon" v.Color = Color3.fromRGB(0,0,0) v.Transparency = 0.32 end
+    if v:IsA("BasePart") then
+        if v.Name == "HumanoidRootPart" then
+            v:Destroy()
+        end
+    end
+end
 b.Character = char:Clone()
+local wat = CFrame.new()
+local wat2 = wat
 b.Character.Parent = workspace
 function stepped()
     local sine = (tick() - start) * 30
@@ -128,7 +138,7 @@ function stepped()
     if D then
         PotentialCFrame *= CFrame.new(1,0,0)
     end
-    WalkSpeed = 45
+    WalkSpeed = 75
     if (PotentialCFrame.X ~= OldCFrame.X or PotentialCFrame.Z ~= OldCFrame.Z) then
         Moving = true
         mainpos = mainpos:Lerp(CFrame.new(mainpos.p,PotentialCFrame.p)*CFrame.new(0,0,(tick()-LastFrame)*-(WalkSpeed)), 0.65)
@@ -137,7 +147,7 @@ function stepped()
     end
     LastFrame = tick()
     if mainpos.Y<=-50 then fallingspeed=0 mainpos=mainpos+Vector3.new(0,75,0)end
-    RayProperties.FilterDescendantsInstances = {owner.Character}
+    RayProperties.FilterDescendantsInstances = {b.Character}
     local Closest, __L = math.huge, nil
     local _Ray = nil
     local _Raycasts = {}
@@ -148,14 +158,16 @@ function stepped()
             mainpos = CFrame.new(mainpos.p,mainpos.p+LookVector)
         end
     end
-    table.insert(_Raycasts,workspace:Raycast(mainpos.Position-Vector3.new(0,1,0),Vector3.new(0,-9e9,0),RayProperties))
-    for b,_Raycast in pairs(_Raycasts) do
-        local Magnitude = (mainpos.Position-_Raycast.Position).Magnitude
-        if Magnitude < Closest then
-            Closest,_Ray = Magnitude,_Raycast
+    if not FlyMode then
+        table.insert(_Raycasts,workspace:Raycast(mainpos.Position-Vector3.new(0,1,0),Vector3.new(0,-9e9,0),RayProperties))
+        for b,_Raycast in pairs(_Raycasts) do
+            local Magnitude = (mainpos.Position-_Raycast.Position).Magnitude
+            if Magnitude < Closest then
+                Closest,_Ray = Magnitude,_Raycast
+            end
         end
     end
-    RayProperties.FilterDescendantsInstances = {owner.Character}
+    RayProperties.FilterDescendantsInstances = {b.Character}
     table.clear(_Raycasts)
     if not FlyMode then
         if _Ray then
@@ -190,15 +202,22 @@ function stepped()
         falling = false
     end
     local sn = sine
-    pcall(function()
-        if b.Character then owner.Character=b.Character; end;
-    end)
+    if not Moving then
+        wat = wat:Lerp(CFrame.new(0, 0, 0)*CFrame.Angles(math.rad(-3+1*math.cos(sn/12)),0,0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))), 0.25)
+    else
+        wat = wat:Lerp(CFrame.new(0, 0, 0)*CFrame.Angles(math.rad(0*math.cos(sn/12)),math.rad(2*math.sin(sine/14)),0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))), 0.25)
+    end
+    wat2 = MainPos * wat
     pcall(function()
         if not b.Character.Head:GetAttribute(owner.Name) then
-          b.Character.Head:SetAttribute(owner.Name,true)
+            b.Character.Head:SetAttribute(owner.Name,true)
+        end
+        Remote:FireAllClients("cam",b.Character.Head)
+        if owner.Character then
+            owner.Character:Destroy()
         end
     end)
-    MainPos = MainPos:Lerp(mainpos*CFrame.Angles(0,math.rad(180),0),0.1)
+    MainPos = MainPos:Lerp(mainpos*CFrame.Angles(0,math.rad(0),0),0.045)
     if not b.Character or not NewChar or not     pcall(function()
             for i,v in pairs(b.Character:GetDescendants()) do
                 if v:IsA("Motor6D") then
@@ -208,42 +227,54 @@ function stepped()
                     v:Destroy()
                 end
             end
-            if W or A or S or D then
-                cler({
-                    CFrame.new(0,0-0.1*math.sin(sn/6),0)*CFrame.Angles(0,math.rad(0+2*math.sin(sn/6)),0), 
-                    CFrame.new(0,1.5,0)*CFrame.Angles(0,0,0), 
-                    CFrame.new(-1.5,0-0.05*math.sin(sn/6),0-0.4*math.sin(sn/6))*CFrame.Angles(math.rad(0+30*math.sin(sn/6)),math.rad(-0+4*math.cos(sn/6)),0),
-                    CFrame.new(1.5,0-0.05*math.sin(sn/6),0+0.4*math.sin(sn/6))*CFrame.Angles(math.rad(0-30*math.sin(sn/6)),math.rad(0-4*math.cos(sn/6)),0),
-                    CFrame.new(.5,-2+0.1*math.sin(sn/6),0.1+0.6*math.sin(sn/6))*CFrame.Angles(math.rad(-10-30*math.sin(sn/6)),math.rad(-0+4*math.cos(sn/6)),0),
-                    CFrame.new(-.5,-2+0.1*math.sin(sn/6),0.1-0.6*math.sin(sn/6))*CFrame.Angles(math.rad(-10+30*math.sin(sn/6)),math.rad(0-4*math.cos(sn/6)),0),},1)
+            if FlyMode then
+                b.Character["Torso"].CFrame = MainPos*CFrame.new(4*math.sin(sine/24), 2*math.cos(sine/24), -2*math.sin(sine/24))*CFrame.Angles(0,math.rad(14.3*math.sin(sine/24)),0)
+                b.Character["Right Arm"].CFrame = b.Character["Torso"].CFrame*CFrame.new(1.5,-0.2-0.075*math.sin(sn/36),-0.3)*CFrame.Angles(0,math.rad(-10+1*math.cos(sine/24)),0)
+                b.Character["Left Arm"].CFrame = b.Character["Torso"].CFrame*CFrame.new(-1.5,-0.2-0.075*math.sin(sn/36),-0.3)*CFrame.Angles(0,math.rad(10-1*math.cos(sine/24)),0)
+                b.Character["Right Leg"].CFrame = b.Character["Torso"].CFrame*CFrame.new(0.5,-2,0)*CFrame.Angles(math.rad(0-1*math.sin(sine/24)),math.rad(10-1*math.cos(sine/24)),0)
+                b.Character["Left Leg"].CFrame = b.Character["Torso"].CFrame*CFrame.new(-0.5,-2,-0.4)*CFrame.Angles(math.rad(-14+7*math.sin(sine/24)),math.rad(-10-1*math.cos(sine/24)),0)
+                b.Character["Head"].CFrame = b.Character["Torso"].CFrame*CFrame.new(0,1.5,0)
             else
-                b.Character["Torso"].CFrame = MainPos*CFrame.new(0, thing, 0)
-                b.Character["Right Arm"].CFrame = MainPos*CFrame.new(1.5,thing,0)
-                b.Character["Left Arm"].CFrame = MainPos*CFrame.new(-1.5,thing,0)
-                b.Character["Right Leg"].CFrame = MainPos*CFrame.new(0.5,-2,0)
-                b.Character["Left Leg"].CFrame = MainPos*CFrame.new(-0.5,-2,0)
-                b.Character["Head"].CFrame = MainPos*CFrame.new(0,1.5+thing,0)
+                if not Moving then
+                    b.Character["Torso"].CFrame=b.Character["Torso"].CFrame:Lerp(wat2,1)
+                    b.Character["Head"].CFrame=b.Character["Head"].CFrame:Lerp(wat2*CFrame.new(0, 1.5, 0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                    b.Character["Left Arm"].CFrame=b.Character["Left Arm"].CFrame:Lerp(wat2*CFrame.new(-1.45,-0.2,-0.1)*CFrame.Angles(math.rad(3-1*math.cos(sn/12)),math.rad(-10+1*math.sin(sine/25)),0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                    b.Character["Right Arm"].CFrame=b.Character["Right Arm"].CFrame:Lerp(wat2*CFrame.new(0.5,-0.1,-0.5)*CFrame.Angles(math.rad(90-0.0647777*math.cos(sn/12)),math.rad(10-1*math.sin(sine/25)),math.rad(90))*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                    b.Character["Left Leg"].CFrame=b.Character["Left Leg"].CFrame:Lerp(wat2*CFrame.new(-0.5, -2, 0)*CFrame.Angles(math.rad(3-1*math.cos(sn/12)),math.rad(10),0),1)
+                    b.Character["Right Leg"].CFrame=b.Character["Right Leg"].CFrame:Lerp(wat2*CFrame.new(0.5, -2, 0)*CFrame.Angles(math.rad(3-1*math.cos(sn/12)),math.rad(-10),0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                else
+                    b.Character["Torso"].CFrame=b.Character["Torso"].CFrame:Lerp(wat2,1)
+                    b.Character["Head"].CFrame=b.Character["Head"].CFrame:Lerp(wat2*CFrame.new(0, 1.5, 0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                    b.Character["Left Arm"].CFrame=b.Character["Left Arm"].CFrame:Lerp(wat2*CFrame.new(-1.45,-0.2,-0.1)*CFrame.Angles(math.rad(3-1*math.cos(sn/12)),math.rad(-10+1*math.sin(sine/25)),0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                    b.Character["Right Arm"].CFrame=b.Character["Right Arm"].CFrame:Lerp(wat2*CFrame.new(0.5,-0.1,-0.5)*CFrame.Angles(math.rad(90-0.0647777*math.cos(sn/12)),math.rad(10-1*math.sin(sine/25)),math.rad(90))*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                    b.Character["Left Leg"].CFrame=b.Character["Left Leg"].CFrame:Lerp(wat2*CFrame.new(-0.5, -2, 0.1+0.6*math.sin(sine/14))*CFrame.Angles(math.rad(-5-30*math.sin(sn/14)),math.rad(10),0),1)
+                    b.Character["Right Leg"].CFrame=b.Character["Right Leg"].CFrame:Lerp(wat2*CFrame.new(0.5, -2, 0.1-0.6*math.sin(sine/14))*CFrame.Angles(math.rad(-5+30*math.sin(sn/14)),math.rad(-10),0)*CFrame.Angles(math.rad(math.random(-4,4)),math.rad(math.random(-4,4)),math.rad(math.random(-4,4))),1)
+                end
             end
-            b.Character["Head"].Size = Vector3.new(1,1,2)
+            b.Character["Head"].Size = Vector3.new(1,1,1)
             b.Character["Torso"].Size = Vector3.new(2,2,1)
             b.Character["Right Arm"].Size = Vector3.new(1,2,1)
             b.Character["Left Arm"].Size = Vector3.new(1,2,1)
             b.Character["Right Leg"].Size = Vector3.new(1,2,1)
             b.Character["Left Leg"].Size = Vector3.new(1,2,1)
             for i,v in pairs(b.Character:GetDescendants()) do
-              if v:IsA'Humanoid' then v:Destroy() end
+                if v:IsA'Humanoid' then v:Destroy() end
+                if v:IsA'SpecialMesh' or v:IsA'BodyColors' then v:Destroy() end
+                if v:IsA("BasePart") then v.Material = "Neon" v.Color = Color3.fromRGB(0,0,0) v.Transparency = 0.32 end
                 if v:IsA("BasePart") then
-                    if v.Name == "HumanoidRootPart" then
-                        v:Destroy()
-                    end
-                    v.Transparency = 0
                 end
             end
         end) then
+        for i,v in pairs(Parts) do
+            game:GetService("Debris"):AddItem(v,0)
+            table.remove(Parts,i)
+        end
         game:GetService("Debris"):AddItem(b.Character,0)
         NewChar = char:Clone()
         NewChar.Parent = workspace
-        NewChar.HumanoidRootPart.CFrame = MainPos
+        for i,v in pairs(NewChar:GetDescendants()) do 
+            table.insert(Parts,v)
+        end
         b.Character = NewChar
     end
 end
@@ -253,7 +284,7 @@ game:GetService("RunService").Heartbeat:Connect(stepped)
 
 function chatfunc(m)
     pcall(function()
-        local ModeHolder = Instance.new("BillboardGui",owner.Character.Head)
+        local ModeHolder = Instance.new("BillboardGui",b.Character.Head)
         ModeHolder.LightInfluence = 0
         ModeHolder.Active = false
         ModeHolder.AlwaysOnTop = false
@@ -264,7 +295,7 @@ function chatfunc(m)
         ModeHolder.ResetOnSpawn = true
         ModeHolder.Size = UDim2.new(15,0,2,0)
         local sound = Instance.new("Sound")
-        sound.PlayOnRemove = true
+        sound.PlayOnRemove = false
         sound.SoundId = "rbxassetid://6862971697"
         sound.Volume = 0.5
         sound.Parent = ModeHolder.Parent
@@ -288,7 +319,7 @@ function chatfunc(m)
         local a=game:GetService("TweenService"):Create(ModeHolder, TweenInfo.new(1.8), {StudsOffset=Vector3.new(0,2,0)})
         a:Play()
         wait(1.7)
-        local sus = ""
+        local sus = "["..owner.Name.."] :"
         local mer = game:GetService("RunService").Stepped:Connect(function()
             pcall(function()
                 CMode.Text = sus
@@ -296,7 +327,6 @@ function chatfunc(m)
         end)
         for i = 1, #m do
             sus =  sus.. m:sub(i,i)
-            sound:Play()
             wait()
         end
         wait(1)
@@ -313,4 +343,4 @@ end
 owner.Chatted:connect(function(msg)
     chatfunc(msg)
 end)
-chatfunc("ok script ran (fake char)")
+chatfunc("script ran")
